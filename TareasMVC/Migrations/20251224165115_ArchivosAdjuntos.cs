09e9f8b9829dce0d7237954a809b2ad00dd5edc9
Gustavo Delgado <gdquevedo@gmail.com>
2025-12-26 20:58:43 -0600

    Primer commit limpio - Proyecto TareasMVC

---
+++ TareasMVC/Migrations/20251224165115_ArchivosAdjuntos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class ArchivosAdjuntos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "archivoAdjuntos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TareaId = table.Column<int>(type: "int", nullable: false),
+                    Url = table.Column<string>(type: "varchar(max)", unicode: false, nullable: true),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Orden = table.Column<int>(type: "int", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_archivoAdjuntos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_archivoAdjuntos_Tareas_TareaId",
+                        column: x => x.TareaId,
+                        principalTable: "Tareas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_archivoAdjuntos_TareaId",
+                table: "archivoAdjuntos",
+                column: "TareaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "archivoAdjuntos");
+        }
+    }
+}
---
+++ TareasMVC/Migrations/20251225085408_AdminRol.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS(Select Id from AspNetRoles where Id = 'ed7b3a43-c734-436e-be8f-bedcb27d3738')
+                BEGIN
+	            INSERT AspNetRoles (Id, [Name], [NormalizedName])
+	            VALUES ('ed7b3a43-c734-436e-be8f-bedcb27d3738','admin','ADMIN')
+                END
+                ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE AspNetRoles WHERE Id = 'ed7b3a43-c734-436e-be8f-bedcb27d3738'");
+        }
+    }
+}
